4753cf36c5164e76a968da1849e983f6cc258c15
donatas.lunys <donatas.lunys@insoft.lt>
2021-04-08 16:10:35 +0300

    First try

---
+++ gallery-ui/src/main/java/com/gallery/galleryui/ZkInit.java
@@ -0,0 +1,15 @@
+package com.gallery.galleryui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.util.WebAppInit;
+
+public class ZkInit implements WebAppInit {
+
+    @Override
+    public void init(WebApp webapp) throws Exception {
+
+    }
+}
