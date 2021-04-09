5dafaceffa8eff0e1366c10fe91baab2a93b7b5e
donatas.lunys <donatas.lunys@insoft.lt>
2021-04-09 11:36:57 +0300

    Neveikianti versija

--- gallery-ui/src/main/java/com/gallery/galleryui/GalleryUiApplication.java
+++ gallery-ui/src/main/java/com/gallery/galleryui/GalleryUiApplication.java
@@ -15,33 +15,34 @@ import org.zkoss.zk.ui.http.HttpSessionListener;
 @SpringBootApplication
 public class GalleryUiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GalleryUiApplication.class, args);
-	}
-	// ZK servlets
-	@Bean
-	public ServletRegistrationBean dHtmlLayoutServlet() {
-		Map<String, String> params = new HashMap<>();
-		params.put("update-uri", "/zkau");
-		ServletRegistrationBean reg = new ServletRegistrationBean(new DHtmlLayoutServlet(), "*.zul");
-		reg.setLoadOnStartup(1);
-		reg.setInitParameters(params);
-		return reg;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryUiApplication.class, args);
+    }
 
-	@Bean
-	public ServletRegistrationBean dHtmlUpdateServlet() {
-		Map<String, String> params = new HashMap<>();
-		params.put("update-uri", "/zkau/*");
-		ServletRegistrationBean reg = new ServletRegistrationBean(new DHtmlUpdateServlet(), "/zkau/*");
-		reg.setLoadOnStartup(2);
-		reg.setInitParameters(params);
-		return reg;
-	}
+    // ZK servlets
+    @Bean
+    public ServletRegistrationBean dHtmlLayoutServlet() {
+        Map<String, String> params = new HashMap<>();
+        params.put("update-uri", "/src/main/webapp/WEB-INF/zkau");
+        ServletRegistrationBean reg = new ServletRegistrationBean(new DHtmlLayoutServlet(), "*.zul");
+        reg.setLoadOnStartup(1);
+        reg.setInitParameters(params);
+        return reg;
+    }
 
-	// ZK listeners
-	@Bean
-	public ServletListenerRegistrationBean<HttpSessionListener> httpSessionListener() {
-		return new ServletListenerRegistrationBean<>(new HttpSessionListener());
-	}
+    @Bean
+    public ServletRegistrationBean dHtmlUpdateServlet() {
+        Map<String, String> params = new HashMap<>();
+        params.put("update-uri", "/src/main/webapp/WEB-INF/zkau/*");
+        ServletRegistrationBean reg = new ServletRegistrationBean(new DHtmlUpdateServlet(), "/src/main/webapp/WEB-INF/zkau/*");
+        reg.setLoadOnStartup(2);
+        reg.setInitParameters(params);
+        return reg;
+    }
+
+    // ZK listeners
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionListener> httpSessionListener() {
+        return new ServletListenerRegistrationBean<>(new HttpSessionListener());
+    }
 }
--- gallery-ui/src/main/java/com/gallery/galleryui/ZkInit.java
+++ gallery-ui/src/main/java/com/gallery/galleryui/ZkInit.java
@@ -1,15 +1,12 @@
 package com.gallery.galleryui;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.zkoss.zk.ui.WebApp;
 import org.zkoss.zk.ui.util.WebAppInit;
 
 public class ZkInit implements WebAppInit {
 
     @Override
-    public void init(WebApp webapp) throws Exception {
+    public void init(WebApp webApp) throws Exception {
 
     }
 }
