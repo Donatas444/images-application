778ccdf0c1a67e6c44dc942c68ebb78cd6a724b1
donatas.lunys <donatas.lunys@insoft.lt>
2021-04-27 14:52:06 +0300

    Saving Tag with relationship to Image

--- gallery-bl/src/main/java/com/gallery/service/TagService.java
+++ gallery-bl/src/main/java/com/gallery/service/TagService.java
@@ -1,8 +1,23 @@
 package com.gallery.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gallery.gallerymodel.Tag;
+import com.gallery.repository.TagRepository;
 
 @Service
 public class TagService {
+    ;
+    @Autowired
+    TagRepository tagRepository;
+
+    public void addTag(Tag tag) {
+        tagRepository.save(tag);
+    }
+
+    public void deleteTag(Tag tag) {
+        tagRepository.delete(tag);
+    }
 
 }
